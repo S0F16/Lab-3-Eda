93d5ef182175788b3709374b62c5cee18d177439
S0F16 <sofia.deluca6@outlook.com>
2025-10-21 11:35:59 -0300

    Optimize movie catalog processing in LAB3.java
    Refactor movie catalog processing to remove redundant instances and improve performance measurement.

--- LAB3.java
+++ LAB3.java
@@ -3,28 +3,22 @@ public class LAB3 {
 
     public static void main(String[] args) {
         int i = 100;
+        while(i<=1000){
         MovieCatalog mc1 = new MovieCatalog();
         mc1.generateMovies(i);
-        MovieCatalog mc2 = new MovieCatalog();
-        mc2.generateMovies(i);
-        MovieCatalog mc3 = new MovieCatalog();
-        mc3.generateMovies(i);
+       
+        long t1= System.nanoTime();
+        mc1.getMoviesByDirector("Quentin Tarantino"); 
+        long t2= System.nanoTime();
+        long tiempolineal= (t2-t1); 
         
-        long t1 = System.nanoTime();
-        mc1.sortByAlgorithm("InsertionSort", "rating");
-        long t2 = System.nanoTime();
-        long tiempoInsertion = t2 - t1;
-
-        long t3 = System.nanoTime();
-        mc2.sortByAlgorithm("QuickSort", "rating");
-        long t4 = System.nanoTime();
-        long tiempoQuick = t4 - t3;
-
-        long t5 = System.nanoTime();
-        mc3.sortByAlgorithm("RadixSort", "year");
-        long t6 = System.nanoTime();
-        long tiempoRadix = t6 - t5;
-
-        System.out.println(i + "," + tiempoInsertion + "," + tiempoQuick + "," + tiempoRadix);
+        mc1.sortByAlgorithm("QuickSort", "Director");
+        long t3= System.nanoTime();
+        mc1.getMoviesByDirector("Quentin Tarantino"); 
+        long t4= System.nanoTime();
+        long tiempobinario= (t4-t3); 
+        i+= 100;
+    }
+        System.out.println(i+","+tiempolineal+","+tiempobinario);
     }
 }
